f71b89abd99c3338f09a48c3fdbaf69d37dc50eb
Chloe Umble <chloeumble@gmail.com>
2020-09-02 11:27:54 +0100

    Update magazineFlipper.js
    Prevent moving either direction while animating, give credit to the .js fiddle sourced for this

--- magazineFlipper.js
+++ magazineFlipper.js
@@ -1,17 +1,41 @@
-function rewind(rewindSpeed) {    
-   clearInterval(intervalRewind);
-   var startSystemTime = new Date().getTime();
-   var startVideoTime = video.currentTime;
+// Modified from https://jsfiddle.net/r4zamgeo/
+
+var vid = document.getElementById('video');
+var intervalRewind;
+var elapsed = 0;
+
+$("#backward").click(function() {
+  // Disable moving forwards or backwards while animating
+  $('#backward').prop('disabled', true);
+  $('#forward').prop('disabled', true);
+  clearInterval(intervalRewind);
+  var startSystemTime = new Date().getTime();
+  var startVideoTime = video.currentTime;
    
-   intervalRewind = setInterval(function(){
-       video.playbackRate = 1.0;
-       if(video.currentTime == 0){
-           clearInterval(intervalRewind);
-           video.pause();
-       } else {
-           var elapsed = new Date().getTime()-startSystemTime;
-           log.textContent='Rewind Elapsed: '+elapsed.toFixed(3);
-           video.currentTime = Math.max(startVideoTime - elapsed*rewindSpeed/1000.0, 0);
-       }
-   }, 30);
-}
+  intervalRewind = setInterval(function(){
+    if(video.currentTime == 0 || elapsed >= 2000){
+      clearInterval(intervalRewind);
+      video.pause();
+      elapsed = 0;
+     // Re-enable moving forwards or backwards after animating
+      $('#backward').prop('disabled', false);
+      $('#forward').prop('disabled', false);
+    } else {
+      elapsed = new Date().getTime()-startSystemTime;
+      video.currentTime = Math.max(startVideoTime - elapsed/1000.0, 0);
+    }
+  }, 40);
+});
+
+$("#forward").click(function() {
+  // Disable moving forwards or backwards while animating
+  $('#forward').prop('disabled', true);
+  $('#backward').prop('disabled', true);
+   
+  vid.play();
+  setTimeout(function(){
+    vid.pause()}, 2000);
+    // Re-enable moving forwards or backwards after animating
+    $('#forward').prop('disabled', false);
+    $('#backward').prop('disabled', false);
+});
